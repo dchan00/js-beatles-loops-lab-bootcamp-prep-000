cc7044b1689af8efa47efdefe155f5391df69e23
DANIEL CHAN <dchan513@msn.com>
2017-09-16 02:28:54 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function theBeatlesPlay(musicians, instruments) {
+  var pair = {}
+
+  for(vaar i=0; i<musicians.length; i++)
+  {
+      var a = "${musicians[i]} plays ${instruments[i]}"
+      pair.push(a)
+  }
+}
